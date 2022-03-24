3a672ad98cce70e67d387ad3a65e87fe2a5daf02
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-03-24 18:21:05 +0530

    callback changes

--- core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetCallbackHandler.java
+++ core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetCallbackHandler.java
@@ -257,7 +257,7 @@ public class NovalnetCallbackHandler implements BeforeControllerHandlerAdaptee {
             Map<String, String[]> paymentTypes = new HashMap<String, String[]>();
             String[] creditCardPaymentTypes = {"CREDITCARD", "CREDITCARD_CHARGEBACK", "CREDITCARD_BOOKBACK", "TRANSACTION_CANCELLATION", "CREDIT_ENTRY_CREDITCARD", "DEBT_COLLECTION_CREDITCARD"};
             String[] directDebitSepaPaymentTypes = {"DIRECT_DEBIT_SEPA", "RETURN_DEBIT_SEPA", "REFUND_BY_BANK_TRANSFER_EU", "TRANSACTION_CANCELLATION", "CREDIT_ENTRY_SEPA", "DEBT_COLLECTION_SEPA"};
-            String[] invoicePaymentTypes = {"INVOICE_START", "INVOICE_CREDIT", "TRANSACTION_CANCELLATION", "REFUND_BY_BANK_TRANSFER_EU", "CREDIT_ENTRY_DE", "DEBT_COLLECTION_DE"};
+            String[] invoicePaymentTypes = {"INVOICE_START", "INVOICE_CREDIT", "TRANSACTION_CANCELLATION", "REFUND_BY_BANK_TRANSFER_EU", "CREDIT_ENTRY_DE", "DEBT_COLLECTION_DE", "INVOICE"};
             String[] prepaymentPaymentTypes = {"INVOICE_START", "INVOICE_CREDIT", "REFUND_BY_BANK_TRANSFER_EU", "CREDIT_ENTRY_DE", "DEBT_COLLECTION_DE"};
             String[] multibancoPaymentTypes = {"MULTIBANCO", "MULTIBANCO_CREDIT"};
             String[] payPalPaymentTypes = {"PAYPAL", "PAYPAL_BOOKBACK", "REFUND_BY_BANK_TRANSFER_EU"};
