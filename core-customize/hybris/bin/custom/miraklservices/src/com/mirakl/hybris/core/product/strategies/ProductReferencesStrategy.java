65d04f812df54361efbde5758ccf2d22375d7198
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-04-29 11:38:39 +0530

    remove novalnet

--- core-customize/hybris/bin/custom/miraklservices/src/com/mirakl/hybris/core/product/strategies/ProductReferencesStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.mirakl.hybris.core.product.strategies;
-
-import de.hybris.platform.core.model.product.ProductModel;
-
-public interface ProductReferencesStrategy {
-
-  String getProductReferences(ProductModel productModel);
-}
