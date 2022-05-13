19242f68f25258c27556dfb49aaceeb3cef30c66
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-05-13 11:56:47 +0530

    install novalnet occ

---
+++ core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/NovalnetoccController.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package novalnet.controllers;
+
+public class NovalnetoccController
+{
+	// implement the controller here
+}
