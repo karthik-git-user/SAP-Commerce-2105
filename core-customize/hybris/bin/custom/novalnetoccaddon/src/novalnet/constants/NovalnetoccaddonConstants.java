d06a3d79b96bac083284e61e0959ecc9fca3d04d
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-05-13 15:52:17 +0530

    install novalnetoccaddon

---
+++ core-customize/hybris/bin/custom/novalnetoccaddon/src/novalnet/constants/NovalnetoccaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package novalnet.constants;
+
+/**
+ * Global class for all novalnetoccaddon constants. You can add global constants for your extension into this class.
+ */
+@SuppressWarnings({ "deprecation", "squid:CallToDeprecatedMethod" })
+public final class NovalnetoccaddonConstants extends GeneratedNovalnetoccaddonConstants
+{
+	public static final String EXTENSIONNAME = "novalnetoccaddon"; //NOSONAR
+
+	private NovalnetoccaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
