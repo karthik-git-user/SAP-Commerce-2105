8ccd3409f53ca76318c0209649234ef5f63c9912
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-05-19 13:01:32 +0530

    facade

--- core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetCartsController.java
+++ core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetCartsController.java
@@ -80,7 +80,8 @@ public class NovalnetCartsController
 	private CheckoutFacade checkoutFacade;
 	@Resource(name = "sopPaymentDetailsValidator")
 	private SopPaymentDetailsValidator sopPaymentDetailsValidator;
-
+	@Resource(name = "novalnetFacade")
+    NovalnetFacade novalnetFacade;
 	
 
 	@RequestMapping(value = "/{cartId}/payment/sop/request", method = RequestMethod.GET)
