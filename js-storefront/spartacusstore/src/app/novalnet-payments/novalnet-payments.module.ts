cc0bf5734bc59cca99610554880f2d2be557f4ce
karthik-git-user <karthikkarthi7299@gmail.com>
2022-05-20 02:31:52 +0530

    integration

---
+++ js-storefront/spartacusstore/src/app/novalnet-payments/novalnet-payments.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaymentDetailsFormComponent } from './payment-details-form/payment-details-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaymentDetailsFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NovalnetPaymentsModule { }
