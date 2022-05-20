12e524d766cd2ff896e0bb0e65f99f8dcee7f538
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-05-20 11:19:09 +0530

    spa

--- js-storefront/spartacusstore/src/app/novalnet-payments/novalnet-payments.module.ts
+++ js-storefront/spartacusstore/src/app/novalnet-payments/novalnet-payments.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentDetailsFormComponent } from './payment-details-form/payment-details-form.component';
-import { CmsConfig, ConfigModule } from '@spartacus/core';
-
-
+import { CardType, CmsConfig, ConfigModule, PaymentDetails } from '@spartacus/core';
+import { CheckoutPaymentAdapter } from '@spartacus/checkout/core';
+import { Observable } from 'rxjs';
 
 @NgModule({
   declarations: [
     PaymentDetailsFormComponent
   ],
   imports: [
-    CommonModule,
-    ConfigModule.withConfig({
-      cmsComponents : {
-        CheckoutPaymentDetails : {
-          component : PaymentDetailsFormComponent
-        }
-      }
-    } as CmsConfig),
+    CommonModule
   ]
 })
-export class NovalnetPaymentsModule { }
+export class NovalnetPaymentsModule implements CheckoutPaymentAdapter  {
+  create(userId: string, cartId: string, paymentDetails: PaymentDetails): Observable<PaymentDetails> {
+	  console.log("test18+++++++++++");    
+    throw new Error('Method not implemented.');
+  }
+  set(userId: string, cartId: string, paymentDetailsId: string): Observable<any> {
+	  console.log("test22+++++++++++");
+    throw new Error('Method not implemented.');
+  }
+  loadCardTypes(): Observable<CardType[]> {
+	  console.log("test26+++++++++++");
+    throw new Error('Method not implemented.');
+  }
+}
