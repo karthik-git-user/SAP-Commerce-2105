bc81d964528a7de8ad655823b43b55642a698232
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-06-02 20:05:18 +0530

    ovv

--- core-customize/hybris/bin/custom/novalnetfacades/src/de/novalnet/order/NovalnetOrderFacade.java
+++ core-customize/hybris/bin/custom/novalnetfacades/src/de/novalnet/order/NovalnetOrderFacade.java
@@ -1,7 +1,6 @@
-/*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
- */
-package novalnet.controllers.v2;
+package de.novalnet.order;
+
+import java.util.List;
 
 import de.hybris.platform.commercefacades.order.data.CCPaymentInfoData;
 import de.hybris.platform.commercewebservicescommons.dto.order.PaymentDetailsWsDTO;
@@ -12,7 +11,6 @@ import de.hybris.platform.webservicescommons.swagger.ApiFieldsParam;
 import novalnet.controllers.InvalidPaymentInfoException;
 import novalnet.controllers.NoCheckoutCartException;
 import novalnet.controllers.UnsupportedRequestException;
-//~ import de.hybris.platform.commercewebservices.core.request.support.impl.PaymentProviderRequestSupportedStrategy;
 import de.hybris.platform.commerceservices.request.mapping.annotation.RequestMappingOverride;
 
 import javax.annotation.Resource;
@@ -46,6 +44,8 @@ import de.hybris.platform.commercefacades.user.data.AddressData;
 import de.hybris.platform.commercefacades.user.data.CountryData;
 import de.hybris.platform.commercefacades.user.data.RegionData;
 import de.hybris.platform.commerceservices.strategies.CheckoutCustomerStrategy;
+import de.hybris.platform.commerceservices.customer.CustomerAccountService;
+import de.hybris.platform.commerceservices.service.data.CommerceCheckoutParameter;
 import de.hybris.platform.commercewebservicescommons.dto.order.PaymentDetailsListWsDTO;
 import de.hybris.platform.commercewebservicescommons.dto.order.PaymentDetailsWsDTO;
 import de.hybris.platform.converters.Populator;
@@ -69,6 +69,7 @@ import de.hybris.platform.servicelayer.i18n.CommonI18NService;
 import de.hybris.platform.servicelayer.keygenerator.KeyGenerator;
 import de.hybris.platform.servicelayer.model.ModelService;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
+
 import de.hybris.platform.servicelayer.session.SessionService;
 import de.hybris.platform.store.BaseStoreModel;
 import de.hybris.platform.store.services.BaseStoreService;
@@ -95,17 +96,12 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+/**
+ * Facade for setting shipping options on marketplace order entries
+ */
+public class NovalnetOrderFacade {
 
-import de.novalnet.order.NovalnetOrderFacade;
-
-
-@Controller
-@RequestMapping(value = "/{baseSiteId}/users/{userId}/carts")
-@CacheControl(directive = CacheControlDirective.NO_CACHE)
-@Api(tags = "Cart Payments")
-public class NovalnetCartPaymentsController
-{
-	private final static Logger LOG = Logger.getLogger(NovalnetCartPaymentsController.class);
+	private final static Logger LOG = Logger.getLogger(NovalnetOrderFacade.class);
 	
 	private BaseStoreService baseStoreService;
     private SessionService sessionService;
@@ -114,8 +110,6 @@ public class NovalnetCartPaymentsController
     private CheckoutFacade checkoutFacade;
     private CheckoutCustomerStrategy checkoutCustomerStrategy;
     private ModelService modelService;
-    //~ private PaymentsResponseConverter paymentsResponseConverter;
-    //~ private PaymentsDetailsResponseConverter paymentsDetailsResponseConverter;
     private FlexibleSearchService flexibleSearchService;
     private Converter<AddressData, AddressModel> addressReverseConverter;
     private Converter<CountryModel, CountryData> countryConverter;
@@ -124,98 +118,12 @@ public class NovalnetCartPaymentsController
     private CalculationService calculationService;
     private Populator<AddressModel, AddressData> addressPopulator;
     private CommonI18NService commonI18NService;
+    private CustomerAccountService customerAccountService;
     
     @Resource(name = "i18NFacade")
     private I18NFacade i18NFacade;
     
-    @Resource(name = "novalnetOrderFacade")
-    NovalnetOrderFacade novalnetOrderFacade;
-	
-	private static final String PAYMENT_MAPPING = "accountHolderName,cardNumber,cardType,cardTypeData(code),expiryMonth,expiryYear,issueNumber,startMonth,startYear,subscriptionId,defaultPaymentInfo,saved,billingAddress(titleCode,firstName,lastName,line1,line2,town,postalCode,country(isocode),region(isocode),defaultAddress)";
-
-	//~ @Resource(name = "paymentProviderRequestSupportedStrategy")
-	//~ private PaymentProviderRequestSupportedStrategy paymentProviderRequestSupportedStrategy;
-
-	@Secured({ "ROLE_CUSTOMERGROUP", "ROLE_GUEST", "ROLE_CUSTOMERMANAGERGROUP", "ROLE_TRUSTED_CLIENT" })
-	@PostMapping(value = "/{cartId}/paymentdetails", consumes = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE })
-	@ResponseStatus(HttpStatus.CREATED)
-	@RequestMappingOverride
-	@ResponseBody
-	@ApiOperation(nickname = "createCartPaymentDetails", value = "Defines and assigns details of a new credit card payment to the cart.", notes = "Defines the details of a new credit card, and assigns this payment option to the cart.")
-	@ApiBaseSiteIdUserIdAndCartIdParam
-	public void createCartPaymentDetails(@ApiParam(value =
-			"Request body parameter that contains details such as the name on the card (accountHolderName), the card number (cardNumber), the card type (cardType.code), "
-					+ "the month of the expiry date (expiryMonth), the year of the expiry date (expiryYear), whether the payment details should be saved (saved), whether the payment details "
-					+ "should be set as default (defaultPaymentInfo), and the billing address (billingAddress.firstName, billingAddress.lastName, billingAddress.titleCode, billingAddress.country.isocode, "
-					+ "billingAddress.line1, billingAddress.line2, billingAddress.town, billingAddress.postalCode, billingAddress.region.isocode)\n\nThe DTO is in XML or .json format.", required = true) @RequestBody final PaymentDetailsWsDTO paymentDetails,
-			@ApiFieldsParam @RequestParam(defaultValue = "Default") final String fields)
-			throws InvalidPaymentInfoException, NoCheckoutCartException, UnsupportedRequestException
-	{
-		LOG.info("+++++++++98++++++++");
-		final AddressModel billingAddress = novalnetOrderFacade.createBillingAddress(paymentDetails);
-		NovalnetPaymentInfoModel paymentInfo = createPaymentInfo(paymentDetails);
-		paymentInfo.setBillingAddress(billingAddress);
-        billingAddress.setOwner(paymentInfo);
-        paymentInfoData = novalnetOrderFacade.addPaymentDetailsInternal(paymentInfo).getPaymentInfo();
-        LOG.info("+++++++++161++++++++");
-        LOG.info(paymentInfoData);
-        LOG.info("+++++++++163++++++++");
-	}
-	
-	public NovalnetPaymentInfoModel createPaymentInfo(PaymentDetailsWsDTO paymentDetails) {
-        NovalnetPaymentInfoModel paymentInfoModel = new NovalnetPaymentInfoModel();
-        CustomerModel customerModel = getCheckoutCustomerStrategy().getCurrentUserForCheckout();
-		paymentInfoModel.setPaymentEmailAddress("karthik_m@novalnetsolutions.com");
-		paymentInfoModel.setDuplicate(Boolean.FALSE);
-		paymentInfoModel.setSaved(Boolean.TRUE);
-		paymentInfoModel.setUser(customerModel);
-		paymentInfoModel.setPaymentInfo("notes");
-		paymentInfoModel.setOrderHistoryNotes("notes");
-		paymentInfoModel.setPaymentProvider("NovalnetCreditCard");
-		paymentInfoModel.setPaymentGatewayStatus("pending");
-        return paymentInfoModel;
-    }
-	
-	private AddressModel createBillingAddress(PaymentDetailsWsDTO paymentDetails) {
-        String titleCode = paymentDetails.getBillingAddress().getTitleCode();
-        final AddressModel billingAddress = getModelService().create(AddressModel.class);
-        if (StringUtils.isNotBlank(titleCode)) {
-            final TitleModel title = new TitleModel();
-            title.setCode(titleCode);
-            billingAddress.setTitle(getFlexibleSearchService().getModelByExample(title));
-        }
-        billingAddress.setFirstname(paymentDetails.getBillingAddress().getFirstName());
-        billingAddress.setLastname(paymentDetails.getBillingAddress().getLastName());
-        billingAddress.setLine1(paymentDetails.getBillingAddress().getLine1());
-        billingAddress.setLine2(paymentDetails.getBillingAddress().getLine2());
-        billingAddress.setTown(paymentDetails.getBillingAddress().getTown());
-        billingAddress.setPostalcode(paymentDetails.getBillingAddress().getPostalCode());
-        billingAddress.setCountry(getCommonI18NService().getCountry(paymentDetails.getBillingAddress().getCountry().getIsocode()));
-
-        final AddressData addressData = new AddressData();
-        addressData.setTitleCode(paymentDetails.getBillingAddress().getTitleCode());
-        addressData.setFirstName(billingAddress.getFirstname());
-        addressData.setLastName(billingAddress.getLastname());
-        addressData.setLine1(billingAddress.getLine1());
-        addressData.setLine2(billingAddress.getLine2());
-        addressData.setTown(billingAddress.getTown());
-        addressData.setPostalCode(billingAddress.getPostalcode());
-        addressData.setBillingAddress(true);
-
-        if (paymentDetails.getBillingAddress().getCountry() != null) {
-            final CountryData countryData = getI18NFacade().getCountryForIsocode(paymentDetails.getBillingAddress().getCountry().getIsocode());
-            addressData.setCountry(countryData);
-        }
-        if (paymentDetails.getBillingAddress().getRegion().getIsocode() != null) {
-            final RegionData regionData = getI18NFacade().getRegion(paymentDetails.getBillingAddress().getCountry().getIsocode(), paymentDetails.getBillingAddress().getRegion().getIsocode());
-            addressData.setRegion(regionData);
-        }
-
-        getAddressReverseConverter().convert(addressData, billingAddress);
-
-        return billingAddress;
-    }
+    
 	
 	public BaseStoreService getBaseStoreService() {
         return baseStoreService;
@@ -344,6 +252,113 @@ public class NovalnetCartPaymentsController
 
     public void setAddressPopulator(Populator<AddressModel, AddressData> addressPopulator) {
         this.addressPopulator = addressPopulator;
+    }
+    
+    protected CartData addPaymentDetailsInternal(final NovalnetPaymentInfoModel paymentInfo) throws InvalidPaymentInfoException
+	{
+		final CustomerModel currentCustomer = getCurrentUserForCheckout();
+		getCustomerAccountService().setDefaultPaymentInfo(currentCustomer, paymentInfo);
+		final CartModel cartModel = getCart();
+		final CommerceCheckoutParameter parameter = createCommerceCheckoutParameter(cartModel, true);
+		parameter.setPaymentInfo(ccPaymentInfoModel);
+		getCommerceCheckoutService().setPaymentInfo(parameter);
+		return cartFacade.getSessionCart();
+	}
+	
+	protected CustomerModel getCurrentUserForCheckout()
+	{
+		return getCheckoutCustomerStrategy().getCurrentUserForCheckout();
+	}
+	
+	protected CommerceCheckoutParameter createCommerceCheckoutParameter(final CartModel cart, final boolean enableHooks)
+	{
+		final CommerceCheckoutParameter parameter = new CommerceCheckoutParameter();
+		parameter.setEnableHooks(enableHooks);
+		parameter.setCart(cart);
+		return parameter;
+	}
+	
+	protected CustomerAccountService getCustomerAccountService()
+	{
+		return customerAccountService;
+	}
+
+	@Required
+	public void setCustomerAccountService(final CustomerAccountService customerAccountService)
+	{
+		this.customerAccountService = customerAccountService;
+	}
+	
+	protected <T extends CartService> T getCartService()
+	{
+		return (T) cartService;
+	}
+	
+	@Override
+	public boolean hasCheckoutCart()
+	{
+		return getCartFacade().hasSessionCart();
+	}
+
+	protected CartModel getCart()
+	{
+		return hasCheckoutCart() ? getCartService().getSessionCart() : null;
+	}
+	
+	protected CartData getSessionCart()
+	{
+		return cartFacade.getSessionCart();
+	}
+	
+	protected CommerceCheckoutService getCommerceCheckoutService()
+	{
+		return commerceCheckoutService;
+	}
+
+	@Required
+	public void setCommerceCheckoutService(final CommerceCheckoutService commerceCheckoutService)
+	{
+		this.commerceCheckoutService = commerceCheckoutService;
+	}
+	
+	private AddressModel createBillingAddress(PaymentDetailsWsDTO paymentDetails) {
+        String titleCode = paymentDetails.getBillingAddress().getTitleCode();
+        final AddressModel billingAddress = getModelService().create(AddressModel.class);
+        if (StringUtils.isNotBlank(titleCode)) {
+            final TitleModel title = new TitleModel();
+            title.setCode(titleCode);
+            billingAddress.setTitle(getFlexibleSearchService().getModelByExample(title));
+        }
+        billingAddress.setFirstname(paymentDetails.getBillingAddress().getFirstName());
+        billingAddress.setLastname(paymentDetails.getBillingAddress().getLastName());
+        billingAddress.setLine1(paymentDetails.getBillingAddress().getLine1());
+        billingAddress.setLine2(paymentDetails.getBillingAddress().getLine2());
+        billingAddress.setTown(paymentDetails.getBillingAddress().getTown());
+        billingAddress.setPostalcode(paymentDetails.getBillingAddress().getPostalCode());
+        billingAddress.setCountry(getCommonI18NService().getCountry(paymentDetails.getBillingAddress().getCountry().getIsocode()));
+
+        final AddressData addressData = new AddressData();
+        addressData.setTitleCode(paymentDetails.getBillingAddress().getTitleCode());
+        addressData.setFirstName(billingAddress.getFirstname());
+        addressData.setLastName(billingAddress.getLastname());
+        addressData.setLine1(billingAddress.getLine1());
+        addressData.setLine2(billingAddress.getLine2());
+        addressData.setTown(billingAddress.getTown());
+        addressData.setPostalCode(billingAddress.getPostalcode());
+        addressData.setBillingAddress(true);
+
+        if (paymentDetails.getBillingAddress().getCountry() != null) {
+            final CountryData countryData = getI18NFacade().getCountryForIsocode(paymentDetails.getBillingAddress().getCountry().getIsocode());
+            addressData.setCountry(countryData);
+        }
+        if (paymentDetails.getBillingAddress().getRegion().getIsocode() != null) {
+            final RegionData regionData = getI18NFacade().getRegion(paymentDetails.getBillingAddress().getCountry().getIsocode(), paymentDetails.getBillingAddress().getRegion().getIsocode());
+            addressData.setRegion(regionData);
+        }
+
+        getAddressReverseConverter().convert(addressData, billingAddress);
+
+        return billingAddress;
     }
     
 }
