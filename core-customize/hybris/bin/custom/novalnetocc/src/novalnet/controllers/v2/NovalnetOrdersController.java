b1e5ad251fb40d3364aad8ee2552a6100f3391bd
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-06-20 20:32:43 +0530

    occ

--- core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetOrdersController.java
+++ core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetOrdersController.java
@@ -523,7 +523,7 @@ public class NovalnetOrdersController
 	@SiteChannelRestriction(allowedSiteChannelsProperty = API_COMPATIBILITY_B2C_CHANNELS)
 	@ApiOperation(nickname = "placeOrder", value = "Place a order.", notes = "Authorizes the cart and places the order. The response contains the new order data.")
 	@ApiBaseSiteIdAndUserIdParam
-	public byte getRedirectURL(
+	public byte[] getRedirectURL(
 			@ApiParam(value = "Cart code for logged in user, cart GUID for guest checkout", required = true) @RequestParam final String cartId,
 			@ApiParam(value = "credit card hash", required = true) @RequestParam final String panHash,
 			@ApiParam(value = "credit card hash", required = true) @RequestParam final String uniqId,
