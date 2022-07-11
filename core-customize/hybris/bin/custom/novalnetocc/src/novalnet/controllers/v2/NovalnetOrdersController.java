9dba5214b180504d521c9f8d4c664556f3397b6f
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-07-11 18:49:26 +0530

    occ

--- core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetOrdersController.java
+++ core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/v2/NovalnetOrdersController.java
@@ -84,6 +84,7 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.net.MalformedURLException;
 
@@ -907,7 +908,25 @@ LOG.info(response.toString());
         
         Gson gson = new GsonBuilder().create();
         String jsonString = gson.toJson(dataParameters);
-        return jsonString.toString();
+
+        String message;
+		JSONObject json = new JSONObject();
+		json.put("name", "student");
+
+		JSONArray array = new JSONArray();
+		JSONObject item = new JSONObject();
+		item.put("information", "test");
+		item.put("id", 3);
+		item.put("name", "course1");
+		array.put(item);
+
+		json.put("course", array);
+
+		message = json.toString();
+
+
+
+        return message;
 	}
 
 
