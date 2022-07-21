6d8fcd1afb27bf2d6591774d5025d2002fd0aab7
“karthik-git-user” <karthik_m@novalnetsolutions.com>
2022-07-21 11:43:46 +0530

    nn

--- core-customize/hybris/bin/custom/novalnetocc/src/novalnet/controllers/NovalnetoccController.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
- */
-package novalnet.controllers;
-
-public class NovalnetoccController
-{
-	// implement the controller here
-}
