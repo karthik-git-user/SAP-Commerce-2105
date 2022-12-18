afe68755425d5e4210a81e4106260209696ed1ee
karthik-git-user <karthikkarthi7299@gmail.com>
2022-12-18 23:50:38 +0530

    occ

--- js-storefront/spartacusstore/src/app/novalnet-payments/checkout.adapter.ts
+++ js-storefront/spartacusstore/src/app/novalnet-payments/checkout.adapter.ts
@@ -1,30 +1,23 @@
+import {
+  PaymentDetails,
+  CheckoutPaymentAdapter,
+  CardType
+} from "@spartacus/core";
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { CardType, PaymentDetails } from '@spartacus/cart/root';
-import { CheckoutPaymentAdapter } from '@spartacus/checkout/core';
-import { ConverterService, OccEndpointsService } from '@spartacus/core';
-import { Observable } from 'rxjs';
-import * as i0 from "@angular/core";
+import { Observable, of } from "rxjs";
 
-export declare class NovalnetCheckoutPaymentAdapter implements CheckoutPaymentAdapter {
-    protected http: HttpClient;
-    protected occEndpoints: OccEndpointsService;
-    protected converter: ConverterService;
-    constructor(http: HttpClient, occEndpoints: OccEndpointsService, converter: ConverterService);
-    private domparser;
-    createPaymentDetails(userId: string, cartId: string, paymentDetails: PaymentDetails): Observable<PaymentDetails>;
-    setPaymentDetails(userId: string, cartId: string, paymentDetailsId: string): Observable<unknown>;
-    protected getSetPaymentDetailsEndpoint(userId: string, cartId: string, paymentDetailsId: string): string;
-    getPaymentCardTypes(): Observable<CardType[]>;
-    protected getPaymentCardTypesEndpoint(): string;
-    protected getProviderSubInfo(userId: string, cartId: string): Observable<any>;
-    protected getPaymentProviderSubInfoEndpoint(userId: string, cartId: string): string;
-    protected createSubWithProvider(postUrl: string, parameters: any): Observable<any>;
-    protected createDetailsWithParameters(userId: string, cartId: string, parameters: any): Observable<PaymentDetails>;
-    protected getCreatePaymentDetailsEndpoint(userId: string, cartId: string): string;
-    private getParamsForPaymentProvider;
-    private extractPaymentDetailsFromHtml;
-    private convertToMap;
-    static ɵfac: i0.ɵɵFactoryDeclaration<NovalnetCheckoutPaymentAdapter, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<NovalnetCheckoutPaymentAdapter>;
+@Injectable()
+export class NovalnetCheckoutPaymentAdapter
+  implements CheckoutPaymentAdapter {
+  constructor() {}
+
+  public create(
+    userId: string,
+    cartId: string,
+    paymentDetails: PaymentDetails
+  ): Observable<PaymentDetails> {
+    // Cybersource-based logic to create payment details
+  }
+
+  // Add other methods to get endpoints, map payment fields, and so on, as needed.
 }
