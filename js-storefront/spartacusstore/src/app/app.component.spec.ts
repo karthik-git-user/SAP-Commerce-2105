41ea9121ba52a4c581a2de7e32afec86a749bedd
karthik <karthik_m@novalnetsolutions.com>
2022-12-25 12:18:06 +0530

    spa

--- js-storefront/spartacusstore/src/app/app.component.spec.ts
+++ js-storefront/spartacusstore/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mystore2'`, () => {
+  it(`should have as title 'mystore1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mystore2');
+    expect(app.title).toEqual('mystore1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mystore2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mystore1 app is running!');
   });
 });
--- js-storefront/spartacusstore/src/app/app.component.ts
+++ js-storefront/spartacusstore/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mystore2';
+  title = 'mystore1';
 }
